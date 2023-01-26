c3490a83fb344988f463cf9407df6569ce28e790
Rajitha Kumara <rajitha@orangehrm.us.com>
2023-01-26 16:57:33 +0530

    Initiate variables

---
+++ variables.tf
@@ -0,0 +1,25 @@
+#Variables declared in this file must be declared in the marketplace.yaml
+#Provide a description to your variables.
+
+############################
+#  Hidden Variable Group   #
+############################
+variable "tenancy_ocid" {
+}
+
+variable "region" {
+}
+
+############################
+#  Compute Configuration   #
+############################
+
+variable "vm_display_name" {
+  description = "Instance Name"
+  default     = "simple-vm"
+}
+
+variable "vm_compute_shape" {
+  description = "Compute Shape"
+  default     = "VM.Standard2.2" //2 cores
+}
