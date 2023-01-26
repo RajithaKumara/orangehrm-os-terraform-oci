e5a456769faff72ab86e0a8a84ed11449c7aa298
Rajitha Kumara <rajitha@orangehrm.us.com>
2023-01-26 17:04:09 +0530

    remove instance_id

--- compute.tf
+++ compute.tf
@@ -1,6 +1,5 @@
 resource "oci_core_image" "test_image" {
     compartment_id = var.compartment_id
-    instance_id = oci_core_instance.test_instance.id
 
     display_name        = var.vm_display_name
     shape               = var.vm_compute_shape
