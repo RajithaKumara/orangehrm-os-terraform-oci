bf67c6d71e4b7d02d8c1e09a0018d9940765ef8a
Rajitha Kumara <rajitha@orangehrm.us.com>
2023-01-27 19:42:45 +0530

    Define availability_domain_number

--- variables.tf
+++ variables.tf
@@ -17,6 +17,16 @@ variable "region" {
 variable "compartment_ocid" {
 }
 
+variable "availability_domain_name" {
+  default     = ""
+  description = "Availability Domain name, if non-empty takes precedence over availability_domain_number"
+}
+
+variable "availability_domain_number" {
+  default     = 1
+  description = "OCI Availability Domains: 1,2,3  (subject to region availability)"
+}
+
 variable "vm_display_name" {
   description = "Instance Name"
   default     = "simple-vm"
