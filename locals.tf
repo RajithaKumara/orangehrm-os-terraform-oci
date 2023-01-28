7fb3a9cd169aee1bcac56fb7526db999f9a83ee8
Rajitha Kumara <rajitha@orangehrm.us.com>
2023-01-28 11:12:53 +0530

    Update check flex shape expression

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
   # Logic to select Oracle Autonomous Linux 7 platform image (version pegged in data source filter)
   platform_image_id = data.oci_core_images.autonomous_ol7.images[0].id
 
-  is_flex_shape = var.vm_compute_shape == "VM.Standard.E3.Flex" ? [var.vm_flex_shape_ocpus] : []
+  is_flex_shape = (var.vm_compute_shape == "VM.Standard3.Flex" || var.vm_compute_shape == "VM.Standard.E3.Flex" || var.vm_compute_shape == "VM.Standard.E4.Flex" || var.vm_compute_shape == "VM.Standard.A1.Flex" || var.vm_compute_shape == "VM.DenseIO.E4.Flex" || var.vm_compute_shape == "VM.Optimized3.Flex") ? [1] : []
 
   is_public_subnet     = true
   use_existing_network = false
